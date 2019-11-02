c8dba6a8ca78c921933a34a0e85bc0e4d4c9d75a
Y J <yjiteaching@outlook.com>
2019-11-02 11:41:11 -0600

    add a new set of files

---
+++ learning_logs/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm, EntryForm
+
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """show all topics"""
+    topiclist=Topic.objects.order_by('date_added')
+    context={'topics':topiclist}
+    return render(request,'learning_logs/topics.html',context)
+
+def topic(request, topic_id):
+    """Show a single topic and its entries."""
+    mytopic = Topic.objects.get(id=topic_id)
+    myentries = mytopic.entry_set.order_by('-date_added')
+    context = {'topic': mytopic, 'entries': myentries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
